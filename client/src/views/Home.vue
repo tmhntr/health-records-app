786c70463665b49a03c78eb31c1cfbce77bc2d3d
Tim Hunter <thunte27@uwo.ca>
2023-02-05 01:03:27 -0500

    add cards to home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>Home page body content</p>
+    <div class="card-container">
+      <Card v-for="card in cards" :key="card.title" :card="card" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Card from "../components/Card.vue";
 
-export default defineComponent({
-  name: "HomePage",
-  components: {},
-});
+export default {
+  components: { Card },
+  data() {
+    return {
+      cards: [
+        { title: "Card 1", content: "Content 1", button_text: "Button 1" },
+        { title: "Card 2", content: "Content 2", button_text: "Button 2" },
+        { title: "Card 3", content: "Content 3", button_text: "Button 3" }
+      ]
+    }
+  }
+};
 </script>
